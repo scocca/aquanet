2b0a58283b034a74f475ee0b14929cf826a06951
scocca <s.cocca.u@gmail.com>
2023-10-16 22:45:47 -0300

    se agrega la primera maquetacion del home, cambiar dimensiones de px a rem

--- my-aquanet/src/App.js
+++ my-aquanet/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import {NavBar} from './components/NavBar.js'
+import {Home} from './components/home.js'
 
 function App() {
   return (
+    <React.Fragment>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Home/>
     </div>
+    </React.Fragment>
   );
 }
 
